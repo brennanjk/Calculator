09cd3272d67c2740bcfa28bfd6ab8baeeff15e0a
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-05-16 09:42:18 -0700

    Create operate function
    Function that will call on our operator functions depending on user input

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b;
 }
+
+function operate (num1, oper, num2) {
+    if (oper === '+') {
+        add(num1, num2);
+    }
+    if (oper === '-') {
+        subtract(num1, num2);
+    }
+    if (oper === '*') {
+        multiply(num1, num2);
+    }
+    if (oper === '/') {
+        divide(num1, num2);
+    }
+}
