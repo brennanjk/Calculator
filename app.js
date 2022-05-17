bce0c6556b6228329952736be8ff6ecded62ab73
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-05-16 23:02:06 -0700

    moving match functions down on js doc

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 
+
+let display = document.querySelector('#display');
+display.textContent = '0';
+
+let buttons = document.querySelectorAll('button')
+
+function change_display(input) {
+    if (display.textContent === '0' || display.textContent === 0) {
+        display.textContent = input;
+    }
+    else {
+        display.textContent += input;
+    }
+}
+
+
 function add(a,b) {
     return a + b;
 }
@@ -28,19 +44,5 @@ function operate (num1, oper, num2) {
     }
     if (oper === '/') {
         return divide(num1, num2);
-    }
-}
-
-let display = document.querySelector('#display');
-display.textContent = '0';
-
-let buttons = document.querySelectorAll('button')
-
-function change_display(input) {
-    if (display.textContent === '0' || display.textContent === 0) {
-        display.textContent = input;
-    }
-    else {
-        display.textContent += input;
     }
 }
